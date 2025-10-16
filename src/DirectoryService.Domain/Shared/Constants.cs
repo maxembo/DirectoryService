0018f2875dd904c9b5deadbee26a285541350183
maxembo <maxim.popov1234@mail.ru>
2025-10-16 18:10:07 +0300

    Создана папка Shared

---
+++ src/DirectoryService.Domain/Shared/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace DirectoryService.Domain.Shared;
+
+public static class Constants
+{
+    public const int MIN_TEXT_LENGTH = 3;
+
+    public const int MAX_DEPARTMENT_NAME_LENGTH = 150;
+
+    public const int MAX_LOCATION_NAME_LENGTH = 120;
+    
+    public const int MAX_POSITION_NAME_LENGTH = 100;
+
+    public const int MAX_POSITION_DESCRIPTION_LENGTH = 1000;
+}
---
+++ src/DirectoryService.Domain/Shared/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace DirectoryService.Domain.Shared;
+
+public abstract class Entity<TId> where TId : notnull
+{
+    protected Entity(TId id) => Id = id;
+
+    public TId Id { get; private set; }
+}
