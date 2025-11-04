56ab4a40fd1a2f8989ff18d50766c9864bd01f39
maxembo <maxim.popov1234@mail.ru>
2025-11-04 22:33:27 +0300

    Добавлена регистрация зависимостей в метод AddProgramDependencies

---
+++ src/DirectoryService.Web/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using DirectoryService.Application;
+using DirectoryService.Infrastructure.Postgres;
+
+namespace DirectoryService.Web;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddProgramDependencies(this IServiceCollection services)
+        => services
+            .AddWebDependencies()
+            .AddInfrastructureDependencies()
+            .AddApplicationDependencies();
+
+    private static IServiceCollection AddWebDependencies(this IServiceCollection services)
+    {
+        services.AddOpenApi();
+        services.AddControllers();
+
+        services.AddLogging();
+
+        return services;
+    }
+}
