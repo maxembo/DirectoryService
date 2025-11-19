3dce64f0625542f4bb761a008c9a76fbb45aa0a8
maxembo <maxim.popov1234@mail.ru>
2025-11-19 17:11:10 +0300

    Зарегистрирован Serilog

--- src/DirectoryService.Web/Program.cs
+++ src/DirectoryService.Web/Program.cs
@@ -1,42 +1,65 @@
+using System.Globalization;
 using DirectoryService.Presentation.Response;
 using DirectoryService.Web;
+using DirectoryService.Web.Middlewares;
 using Microsoft.OpenApi.Models;
+using Serilog;
 using Shared;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddProgramDependencies();
-
-builder.Services.AddOpenApi(
-    options =>
-    {
-        options.AddSchemaTransformer(
-            (schema, context, _) =>
-            {
-                if (context.JsonTypeInfo.Type == typeof(Envelope<Errors>))
-                {
-                    if (schema.Properties.TryGetValue("errors", out var errorsProp))
-                    {
-                        errorsProp.Items.Reference = new OpenApiReference()
-                        {
-                            Type = ReferenceType.Schema,
-                            Id = "Error",
-                        };
-                    }
-                }
-
-                return Task.CompletedTask;
-            });
-    });
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
+    .CreateBootstrapLogger();
+try
 {
-    app.MapOpenApi();
-    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "DirectoryService"));
+    Log.Information("Starting DirectoryService.Web");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddProgramDependencies(builder.Configuration);
+
+    builder.Services.AddOpenApi(
+        options =>
+        {
+            options.AddSchemaTransformer(
+                (schema, context, _) =>
+                {
+                    if (context.JsonTypeInfo.Type == typeof(Envelope<Errors>))
+                    {
+                        if (schema.Properties.TryGetValue("errors", out var errorsProp))
+                        {
+                            errorsProp.Items.Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.Schema, Id = "Error",
+                            };
+                        }
+                    }
+
+                    return Task.CompletedTask;
+                });
+        });
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "DirectoryService"));
+    }
+
+    app.UseExceptionMiddleware();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
