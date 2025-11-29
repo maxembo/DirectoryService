1e95966775c5214a0a74e790a60ea08da2b7e499
maxembo <maxim.popov1234@mail.ru>
2025-11-29 16:37:55 +0300

    Создан репозиторий PositionsRepository

---
+++ src/DirectoryService.Application/Abstractions/IPositionsRepository.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Domain.Positions;
+using Shared;
+
+namespace DirectoryService.Application.Abstractions;
+
+public interface IPositionsRepository
+{
+    Task<Result<Guid, Error>> AddAsync(Position position, CancellationToken cancellationToken = default);
+}
---
+++ src/DirectoryService.Infrastructure.Postgres/Positions/PositionsRepository.cs
@@ -0,0 +1,32 @@
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Application.Abstractions;
+using DirectoryService.Domain.Positions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using Shared;
+
+namespace DirectoryService.Infrastructure.Postgres.Positions;
+
+public class PositionsRepository : IPositionsRepository
+{
+    private readonly DirectoryServiceDbContext _dbContext;
+    private readonly ILogger<PositionsRepository> _logger;
+
+    public PositionsRepository(DirectoryServiceDbContext dbContext, ILogger<PositionsRepository> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<Result<Guid, Error>> AddAsync(Position position, CancellationToken cancellationToken = default)
+    {
+        await _dbContext.AddAsync(position, cancellationToken);
+
+        var saveChangesResult = await _dbContext.SaveChangesResultAsync(cancellationToken);
+        if (saveChangesResult.IsFailure)
+            return saveChangesResult.Error;
+
+        return position.Id.Value;
+    }
+}
