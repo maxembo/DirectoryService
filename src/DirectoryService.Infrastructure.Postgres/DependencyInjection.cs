cbea668bff24a5e890a4afb1f65e1db82a4cc480
maxembo <maxim.popov1234@mail.ru>
2025-11-29 16:39:53 +0300

    Зарегистрированы репозитории

--- src/DirectoryService.Infrastructure.Postgres/DependencyInjection.cs
+++ src/DirectoryService.Infrastructure.Postgres/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using DirectoryService.Application.Abstractions;
 using DirectoryService.Application.Locations;
+using DirectoryService.Infrastructure.Postgres.Departments;
 using DirectoryService.Infrastructure.Postgres.Locations;
+using DirectoryService.Infrastructure.Postgres.Positions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DirectoryService.Infrastructure.Postgres;
@@ -12,6 +14,8 @@ public static class DependencyInjection
         services.AddScoped<DirectoryServiceDbContext>();
 
         services.AddScoped<ILocationsRepository, LocationsRepository>();
+        services.AddScoped<IDepartmentsRepository, DepartmentsRepository>();
+        services.AddScoped<IPositionsRepository, PositionsRepository>();
 
         return services;
     }
