e702ea8f83926b087ef59e598bfe22d8bc2fad51
maxembo <maxim.popov1234@mail.ru>
2025-12-04 00:51:54 +0300

    Создан UpdateDepartment. В домене создан метод обновления locationIds

---
+++ src/DirectoryService.Application/Departments/UpdateDepartments/UpdateDepartmentLocationIdsCommand.cs
@@ -0,0 +1,6 @@
+﻿using DirectoryService.Application.Abstractions;
+using DirectoryService.Contracts.Departments.UpdateDepartment;
+
+namespace DirectoryService.Application.Departments.UpdateDepartments;
+
+public record UpdateDepartmentLocationIdsCommand(Guid DepartmentId, UpdateDepartmentLocationIdsRequest Request) : ICommand;
---
+++ src/DirectoryService.Application/Departments/UpdateDepartments/UpdateDepartmentLocationIdsValidator.cs
@@ -0,0 +1,18 @@
+﻿using DirectoryService.Application.Validation;
+using DirectoryService.Contracts.Departments.UpdateDepartment;
+using FluentValidation;
+using Shared;
+
+namespace DirectoryService.Application.Departments.UpdateDepartments;
+
+public class UpdateDepartmentLocationIdsValidator : AbstractValidator<UpdateDepartmentLocationIdsRequest>
+{
+    public UpdateDepartmentLocationIdsValidator()
+    {
+        RuleFor(u => u.LocationIds)
+            .NotEmpty()
+            .WithError(GeneralErrors.Required("locationIds"))
+            .Must(locationIds => locationIds.Distinct().Count() == locationIds.Length)
+            .WithError(GeneralErrors.ArrayContainsDuplicates("department.locationIds"));
+    }
+}
