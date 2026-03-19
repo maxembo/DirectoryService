3b1184b4a2f9dd6e01d480e00f7171ce1291d614
Maxim Popov <maxim.popov1234@mail.ru>
2026-03-19 16:44:28 +0300

    По умолчанию теперь /home

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/home",
+				permanent: false,
+			},
+		];
+	},
 };
 
 export default nextConfig;
